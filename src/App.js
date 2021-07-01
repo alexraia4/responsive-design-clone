731c555c8a139dde1e047b3dbdf1adf8ec40d0e2
DerpHead <alexraia4@gmail.com>
2021-07-01 13:28:43 -0400

    added some files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import './reset.css'
+import './App.css'
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
+      <div className="App_opacityLayer">
+        <Header/>
+      </div>
       
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import './header.css'
+
+export default function Header() {
+    return (
+      <div className="Header">
+        <p className="Header_title">Start Bootstrap</p>
+        <nav>
+            <p>About</p>
+            <p>Projects</p>
+            <p>Contact</p>
+        </nav>
+      </div>
+    );
+  }
