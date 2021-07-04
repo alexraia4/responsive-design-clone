bd380d2a7a110e88e5cb805afefbb672f9f72114
DerpHead <alexraia4@gmail.com>
2021-07-03 22:31:25 -0400

    finished

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,40 @@
+import React, { useState } from 'react';
 import './header.css'
 
 export default function Header() {
-    return (
-      <div className="Header">
+
+  const [display, setDisplay] = useState(true);
+
+  let changeState = () => {
+    setDisplay(!display)
+    if (display) {
+      document.querySelector(".Header_responsiveMenu").style.display = 'inline'
+    }
+    else {
+      document.querySelector(".Header_responsiveMenu").style.display = 'none'
+    }
+  }
+
+
+
+  return (
+    <div className="Header">
+      <div className='Header_default'>
         <p className="Header_title">Start Bootstrap</p>
         <nav>
-            <p>About</p>
-            <p>Projects</p>
-            <p>Contact</p>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Contact</p>
         </nav>
         <div className='Header_responsiveMenuButton'>
-          <p>Menu</p>
+          <p onClick = {changeState}>Menu</p>
         </div>
       </div>
-    );
-  }
+      <div className="Header_responsiveMenu">
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </div>
+    </div>
+  );
+}
